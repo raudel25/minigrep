5e639b771e523251b43f1ae57d5e2a3ecdbcc076
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-01-12 16:59:22 -0500

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -9,44 +9,39 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    let query = if config.case_sensitive {
-        config.query
-    } else {
-        config.query.to_lowercase()
-    };
-
-    let contents = if config.case_sensitive {
-        contents
-    } else {
-        contents.to_lowercase()
-    };
-
-    for line in search(&query, &contents) {
+    for line in search(&config.query, &contents, !config.case_sensitive) {
         println!("{}", line);
     }
 
     Ok(())
 }
 
-pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+pub fn search<'a>(query: &'a str, contents: &'a str, to_lower: bool) -> Vec<&'a str> {
     let mut results = Vec::new();
+    let query_lower = query.to_lowercase();
 
     for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
+        if to_lower {
+            if line.to_lowercase().contains(&query_lower) {
+                results.push(line);
+            }
+        } else {
+            if line.contains(query) {
+                results.push(line)
+            }
         }
     }
 
     results
 }
 
-pub struct Config {
-    pub query: String,
-    pub filename: String,
+pub struct Config<'a> {
+    pub query: &'a String,
+    pub filename: &'a String,
     pub case_sensitive: bool,
 }
 
-impl Config {
+impl Config<'_> {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
@@ -55,8 +50,8 @@ impl Config {
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
+            query: &args[1],
+            filename: &args[2],
             case_sensitive,
         })
     }

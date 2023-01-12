a5f08eb222c392edc53583afb0966a6828c9e711
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-01-12 12:10:33 -0500

    search and case_sensitive

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
@@ -8,14 +9,41 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    let query = if config.case_sensitive {
+        config.query
+    } else {
+        config.query.to_lowercase()
+    };
+
+    let contents = if config.case_sensitive {
+        contents
+    } else {
+        contents.to_lowercase()
+    };
+
+    for line in search(&query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+
+    results
 }
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -24,9 +52,12 @@ impl Config {
             return Err("not enough arguments");
         }
 
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
         Ok(Config {
             query: args[1].clone(),
             filename: args[2].clone(),
+            case_sensitive,
         })
     }
 }
